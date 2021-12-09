0341c813a2bc2b3487edf3bb5bd187fadb42d36e
LukasMG <70228014+LukasMG@users.noreply.github.com>
2021-12-09 17:45:22 +0000

    Add files via upload

---
+++ Lokaverkefni VESM-3/Wending_machience/Wending_machience/Wending_machience.ino
@@ -0,0 +1,174 @@
+// Adafruit IO Analog Out Example
+// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-analog-output
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+/************************ Example Starts Here *******************************/
+//keypad config
+#include <Keypad.h>
+
+#include <SPI.h>//https://www.arduino.cc/en/reference/SPI
+#include <MFRC522.h>//https://github.com/miguelbalboa/rfid
+
+#define ROW_NUM     4 // four rows
+#define COLUMN_NUM  4 // three columns
+
+#define SS_PIN 5
+#define RST_PIN 22
+
+const int ipaddress[4] = {103, 97, 67, 25};
+
+
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1', '2', '3','A'},
+  {'4', '5', '6','B'},
+  {'7', '8', '9','C'},
+  {'*', '0', '#','D'}
+};
+
+byte nuidPICC[4] = {0, 0, 0, 0};
+MFRC522::MIFARE_Key key;
+MFRC522 rfid = MFRC522(SS_PIN, RST_PIN);
+
+byte pin_rows[ROW_NUM] = {18, 21, 17, 16}; // GIOP18, GIOP5, GIOP17, GIOP16 connect to the row pins
+byte pin_column[COLUMN_NUM] = {4, 0, 2};  // GIOP4, GIOP0, GIOP2 connect to the column pins
+
+Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
+String keyc;
+
+// variables for error function
+int past_error_length = 0;
+int current_error_lenth = 0;
+bool loop_break = false;
+
+// set up the feeds
+AdafruitIO_Feed *error = io.feed("error");
+AdafruitIO_Feed *keypad_input = io.feed("input");
+AdafruitIO_Feed *card = io.feed("cards");
+
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin(115200);
+  Serial.println(F("Initialize System"));
+  //init rfid D8,D5,D6,D7
+  SPI.begin();
+  rfid.PCD_Init();
+
+  Serial.print(F("Reader :"));
+  rfid.PCD_DumpVersionToSerial();
+  // wait for serial monitor to open
+  while(! Serial);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // Function runs whenever a message is received
+ // error->onMessage(errorcheck);
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+  error->get();
+}
+
+void loop() {
+  io.run();
+
+  Serial.println(loop_break);
+  // Reset loop_break after an error code
+  loop_break = false;
+
+  while (loop_break == false){
+    
+    char key = keypad.getKey();
+
+    keyc=(keyc+String(key));
+    delay(500); 
+    if (key == '#'){
+      // Uploads the input from the 4x4
+      Serial.println(keyc);
+      keypad_input->save(keyc);
+      
+      readRFID();
+      
+    }
+  }
+}
+void readRFID(void ) { /* function readRFID */
+  ////Read RFID card
+
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  // Look for new 1 cards
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if (  !rfid.PICC_ReadCardSerial())
+    return;
+
+  // Store NUID into nuidPICC array
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+
+  Serial.print(F("RFID In dec: "));
+  //printDec(rfid.uid.uidByte, rfid.uid.size);
+  Serial.println();
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+
+}
+
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
+}
+
+// this function is called whenever an 'analog' message
+// is received from Adafruit IO. it was attached to
+// the analog feed in the setup() function above.
+void errorcheck(AdafruitIO_Data *data) {
+  int reading = data->toInt();
+
+}
+
